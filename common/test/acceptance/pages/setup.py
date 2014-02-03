216b884d6b49a78e6fbdf7c5d725b9845bc38c83
Will Daly <will@edx.org>
2014-02-03 16:25:53 -0500

    Updated bok choy page objects setup.py

--- common/test/acceptance/pages/setup.py
+++ common/test/acceptance/pages/setup.py
@@ -4,10 +4,18 @@
 Install bok-choy page objects for acceptance and end-to-end tests.
 """
 
+import os
 from setuptools import setup
 
 VERSION = '0.0.1'
 DESCRIPTION = "Bok-choy page objects for edx-platform"
+
+# Pip 1.5 will try to install this package from outside
+# the directory containing setup.py.
+# To protect against this, we change the directory ourselves.
+# This will not affect the parent process, so we do not
+# bother changing it back when we're done.
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 setup(
     name='edxapp-pages',
@@ -25,5 +33,5 @@ setup(
         'Topic :: Software Development :: Testing',
         'Topic :: Software Development :: Quality Assurance'
     ],
-    packages=['edxapp_pages', 'edxapp_pages.lms', 'edxapp_pages.studio']
+    packages=['lms', 'studio']
 )
