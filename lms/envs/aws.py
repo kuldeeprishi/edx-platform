6530bfcc84a1ca7bab339d9ae3971527d767460d
Kuldeep Rishi <kuldeepkrishi@gmail.com>
2014-02-05 12:42:54 +0530

    rai theme updated

--- lms/envs/aws.py
+++ lms/envs/aws.py
@@ -36,8 +36,8 @@ CONFIG_PREFIX = SERVICE_VARIANT + "." if SERVICE_VARIANT else ""
 
 ################################ ALWAYS THE SAME ##############################
 
-DEBUG = False
-TEMPLATE_DEBUG = False
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 EMAIL_BACKEND = 'django_ses.SESBackend'
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
